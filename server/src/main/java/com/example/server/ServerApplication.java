508ed43e5591a783d94493da5b8d15ffdbdd27bf
Roman Nemchinov <Roman_Nemchinov@epam.com>
2018-05-14 16:44:18 +0300

    fix controller

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -8,11 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerApplication{
+public class ServerApplication implements CommandLineRunner {
 	@Autowired
 	private UserRepositiry userRepositiry;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		userRepositiry.save(new User("Valera", "Ivanov"));
+		userRepositiry.save(new User("Kolya", "Petrov"));
+		userRepositiry.save(new User("Ivan", "Ivanov"));
 	}
 }
